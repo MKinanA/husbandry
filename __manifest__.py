c997fae4036362480b064f014da07d049901a54f
MKinanA <mkinanahsan@gmail.com>
2025-09-30 06:39:10 +0700

    Updated dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,16 @@
 {
     'name': 'Poultry and Livestock (Husbandry)',
-    'version': '1.0',
-    'category': 'Custom',
+    # 'version': '1.0',
+    # 'category': 'Custom',
 
     'author': 'Farrell Raafi',
     'website': 'http://odooabc.com',
 
-    'depends': ['base','product', 'om_account_accountant'],
+    'depends': [
+        'base',
+        'product',
+        'om_account_accountant', # https://apps.odoo.com/apps/modules/18.0/om_account_accountant
+    ],
     'data': [
         'security/security.xml',
         'views/husbandry.xml',
