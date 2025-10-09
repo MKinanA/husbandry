7625a01b1f85d1f06d4cf0d204f89e50388933be
MKinanA <mkinanahsan@gmail.com>
2025-10-09 15:33:22 +0700

    Livestock is now associated with a product template and share name and price, and fixed the readonly conditional attribute on certain fields

--- models/livestock.py
+++ models/livestock.py
@@ -13,19 +13,21 @@ states = [
 ]
 exclude_for_readonly = 'draft'
 readonly_on_not_draft_states = {state[0]: [('readonly', True)] for state in states if state[0] != exclude_for_readonly}
+attrs_based_on_state = lambda state, states = readonly_on_not_draft_states: ' '.join((f'{attr[0]}="{int(attr[1]) if isinstance(attr[1], int) else attr[1]}"' for attr in states[state]) if state in states else '')
+xml_readonly_on_not_draft_states = ' or '.join(f'state == \'{state[0]}\'' for state in states if state[0] != exclude_for_readonly) # {'readonly': [*['|'] * (len([state for state in states if state[0] != exclude_for_readonly]) - 1), *[('state', '==', state[0]) for state in states if state[0] != exclude_for_readonly]]}
 
 class HusbandryLivestock(models.Model):
     _name = 'husbandry.livestock'
+    _inherits = {'product.template': 'product_tmpl_id'}
 
-    name = fields.Char(string="Livestock ID", readonly=False, required=True)
     owner_ids = fields.Many2many('res.partner', string="Owner", states=readonly_on_not_draft_states)
     weight = fields.Float(string="Weight (kg)", states=readonly_on_not_draft_states)
     age = fields.Float(string="Age", states=readonly_on_not_draft_states)
     origin = fields.Char(string="Origin", states=readonly_on_not_draft_states)
     vendor_id = fields.Many2one('res.partner', string="Vendor", default=lambda self: self.env.user.partner_id.id, states=readonly_on_not_draft_states)
-    purchase_price = fields.Float(string="Purchase Price (Rp)", states=readonly_on_not_draft_states)
+    purchase_price = fields.Float(related='product_tmpl_id.list_price', string="Purchase Price (Rp)", readonly=False, states=readonly_on_not_draft_states)
     type_id = fields.Many2one('husbandry.type', string="Type", states=readonly_on_not_draft_states)
-    product_id = fields.Many2one('product.product', string="Saleable Product", readonly=True, )
+    product_tmpl_id = fields.Many2one('product.template', string="Saleable Product", readonly=True, required=True)
     image_front = fields.Binary(string="Front Image", states=readonly_on_not_draft_states)
     image_right = fields.Binary(string="Right Image", states=readonly_on_not_draft_states)
     image_left = fields.Binary(string="Left Image", states=readonly_on_not_draft_states)
@@ -61,13 +63,17 @@ class HusbandryLivestock(models.Model):
 
     @api.model
     def create(self, vals):
-        result = super().create(vals)
-        # result.name = str(result.id).zfill(3)
-        return result
+        record = super().create(vals)
+        record.product_tmpl_id.update({
+            'sale_ok': False,
+            'purchase_ok': False,
+            'livestock_id': record.id,
+        })
+        return record
 
     @api.model
     def get_view(self, view_id=None, view_type='form', toolbar=False, submenu=False, **unused_kwargs):
-        print('\nget_view method on HusbandryLivestock called')
+        print(f'\nget_view method on HusbandryLivestock called,\n{self.state = }\n{attrs_based_on_state(self.state) = }\n')
         res = super().get_view(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
         if view_type == 'form':
             res['arch'] = """
@@ -83,38 +89,38 @@ class HusbandryLivestock(models.Model):
                         <button name="create_invoice" string="Buy (coming soon)" type="object" invisible="state != 'saleable'" class="disabled"/>
                         """f"""<field name="state" widget="statusbar" statusbar_visible="{','.join(map(lambda state: state[0], states))}" />""""""
                     </header>
-                    <sheet>
+              """f"""<sheet>
                         <group>
                             <group>
-                                <field name="name"/>
-                                <field name="reference"/>
-                                <field name="weight"/>
-                                <field name="age"/>
-                                <field name="origin"/>
-                                <field name="product_id" invisible="1"/>
-                                <button name="create_product" string="Make Saleable" type="object" invisible="product_id != False"/>
+                                <field name="name" string="Name (Livestock ID)"/>
+                                <field name="reference" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="weight" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="age" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="origin" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="product_tmpl_id" invisible="1"/>
+                                <button name="create_product" string="Make Saleable" type="object" invisible="state != 'draft'"/>
                             </group>
                             <group>
                                 <field name="create_date" string="Registration Date" readonly="1"/>
-                                <field name="owner_ids" widget="many2many_tags"/>
-                                <field name="vendor_id"/>
-                                <field name="kelompok"/>
-                                <field name="type_id"/>
-                                <field name="jatah"/>
-                                <field name="purchase_price" groups="base.group_erp_manager"/>
+                                <field name="owner_ids" widget="many2many_tags" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="vendor_id" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="kelompok" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="type_id" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="jatah" readonly="{xml_readonly_on_not_draft_states}"/>
+                                <field name="purchase_price" groups="base.group_erp_manager" readonly="{xml_readonly_on_not_draft_states}"/>
                             </group>
                         </group>
                         <notebook>
                             <page string="Images">
                                 <group col="4">
-                                    <field name="image_front" widget="image" string="Image 1" options="{'size': [320, 180]}"/>
-                                    <field name="image_right" widget="image" string="Image 2" options="{'size': [320, 180]}"/>
-                                    <field name="image_left" widget="image" string="Image 3" options="{'size': [320, 180]}"/>
-                                    <field name="image_back" widget="image" string="Image 4" options="{'size': [320, 180]}"/>
+                                    <field name="image_front" widget="image" string="Image 1" options="{{'size': [320, 180]}}" readonly="{xml_readonly_on_not_draft_states}"/>
+                                    <field name="image_right" widget="image" string="Image 2" options="{{'size': [320, 180]}}" readonly="{xml_readonly_on_not_draft_states}"/>
+                                    <field name="image_left" widget="image" string="Image 3" options="{{'size': [320, 180]}}" readonly="{xml_readonly_on_not_draft_states}"/>
+                                    <field name="image_back" widget="image" string="Image 4" options="{{'size': [320, 180]}}" readonly="{xml_readonly_on_not_draft_states}"/>
                                 </group>
                             </page>
                         </notebook>
-                    </sheet>
+                    </sheet>""""""
                 </form>
         """
         elif view_type == 'list':
@@ -127,7 +133,7 @@ class HusbandryLivestock(models.Model):
                 """ 
             res['arch'] = res['arch'] + """
                 
-                    <field name="name"/>
+                    <field name="name" string="Name (Livestock ID)"/>
                     <!--<field name="create_date" string="Registration Date" readonly="1"/>-->
                     <field name="weight" string="Berat"/>
                     <field name="vendor_id"/>
@@ -146,7 +152,7 @@ class HusbandryLivestock(models.Model):
                 """ 
             res['arch'] = res['arch'] + """
                 
-                    <field name="name" />
+                    <field name="name" string="Name (Livestock ID)"/>
                     <field name="weight" string="Berat"/>
                     <field name="vendor_id"/>
                     <field name="owner_ids"/>
@@ -177,7 +183,7 @@ class HusbandryLivestock(models.Model):
         elif view_type == 'search':
              res['arch'] = """
                 <search string="Livestock">
-                    <field name="name"/>
+                    <field name="name" string="Name (Livestock ID)"/>
                     <field name="create_date" string="Registration Date" readonly="1"/>
                     <field name="reference"/>
                     <field name="weight" string="Berat"/>
@@ -192,20 +198,19 @@ class HusbandryLivestock(models.Model):
 
     # @api.multi
     def create_product(self):
-        if not self.product_id:
-           name = self.name
-           if self.type_id:
-              name = name + " - " + self.type_id.name
-           category_id = self.env.ref('product.product_category_all')
-           # uom_id = self.env.ref('product.product_uom_unit')
-           self.sudo().product_id = self.sudo().product_id.create({'name': name, 'standard_price': self.purchase_price, 'categ_id': category_id.id, })# 'uom_po_id': uom_id.id, 'uom_id': uom_id.id,})
-           self.state = 'saleable'
-
+        # if not self.product_tmpl_id:
+        #    name = self.name
+        #    if self.type_id:
+        #       name = name + " - " + self.type_id.name
+        #    category_id = self.env.ref('product.product_category_all')
+        #    # uom_id = self.env.ref('product.product_uom_unit')
+        #    self.sudo().product_tmpl_id = self.sudo().product_tmpl_id.create({'name': name, 'list_price': self.purchase_price, 'categ_id': category_id.id, })# 'uom_po_id': uom_id.id, 'uom_id': uom_id.id,})
+        self.state = 'saleable'
 
     # @api.multi
     def create_invoice(self):
         account_id = False
-        product = self.product_id
+        product = self.product_tmpl_id
         if product.id:
             account_id = product.property_account_income_id.id
         if not account_id:
@@ -247,7 +252,7 @@ class HusbandryLivestock(models.Model):
                 'quantity': 1.0,
                 'discount': 0.0,
                 # 'uom_id': product.uom_id.id,
-                'product_id': product.id,
+                'product_tmpl_id': product.id,
             })],
         })
         invoice.compute_taxes()
@@ -275,3 +280,7 @@ class HusbandryLivestock(models.Model):
     # @api.multi
     def paid_invoice(self):
         self.state = 'soldout'
+
+class Product(models.Model):
+    _inherit = 'product.template'
+    livestock_id = fields.Many2one('husbandry.livestock', required=False, ondelete='cascade')
